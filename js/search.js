29e221589b655557c4e54809dbdda67edd783ceb
Ceiceiceii <yz5835@nyu.edu>
2023-12-13 13:00:12 -0500

    updateSearchFunctionFinished

--- js/search.js
+++ js/search.js
@@ -1,3 +1,37 @@
+// JavaScript to highlight the search term and scroll to it in the page
+window.onload = function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const highlightText = urlParams.get('highlight');
+
+    if (highlightText) {
+        const decodedText = decodeURIComponent(highlightText);
+        highlightSearchTerm(decodedText);
+        scrollToFirstHighlight(decodedText);
+    }
+};
+
+function highlightSearchTerm(text) {
+    const bodyContent = document.body.innerHTML;
+    const highlightedContent = bodyContent.replace(new RegExp(escapeRegExp(text), 'gi'), '<span class="highlight">$&</span>');
+    document.body.innerHTML = highlightedContent;
+}
+
+function scrollToFirstHighlight(text) {
+    const highlightedElements = document.querySelectorAll('.highlight');
+    if (highlightedElements.length > 0) {
+        highlightedElements[0].scrollIntoView();
+    }
+}
+
+// Function to escape regular expression special characters in text
+function escapeRegExp(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
+
+function goBack() {
+    window.history.back();
+}
+
 function goBack() {
     window.history.back();
 }
